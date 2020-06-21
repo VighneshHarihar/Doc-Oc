84010cb4875d5c9245900e952eae074fb9418e55
vighnesh <hariharvighnesh@gmail.com>
2020-06-21 15:10:45 +0530

    Linking of Patient-Doctor dashboard

--- app.py
+++ app.py
@@ -1,35 +1,22 @@
 from flask import Flask, render_template, redirect, request
-import QA as qa
+#import QA as qa
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def redirection():
-    return redirect('/home')
+    return redirect('/patientdashboard')
 
 
-@app.route('/home')
-def homePage():
-    return render_template('home.html')
+@app.route('/patientdashboard', methods=['GET', 'POST'])
+def patientdashboard():
+    return render_template('Patientdashboard.html')
 
 
-@app.route('/login')
-def loginPage():
-    return render_template('login.html')
-
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    passage = request.form['para']
-    question = request.form['que']
-    sol = qa.findAnswer(passage, question)
-    return render_template('ans.html', prediction_text='Answer is: {}'.format(sol))
-
-
-@app.route('/ans', methods=['POST'])
-def ans():
-    return render_template('dashboard.html')
+@app.route('/doctordashboard', methods=['GET', 'POST'])
+def doctordashboard():
+    return render_template('DoctorDashboard.html')
 
 
 if __name__ == "__main__":

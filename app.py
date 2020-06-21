b7b8490b99d47e22dcb550c87328a9244e30e243
Vedant Khairnar <vedron007@gmail.com>
2020-06-21 19:52:21 +0530

    Doc Dashboard working

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, redirect, request
 import SpeechToTextMicrophone as sr
-#import textToKeywords as TK
-#import QA as qa
+# import textToKeywords as TK
+# import QA as qa
 import insuranceregression as ir
+
 app = Flask(__name__)
+
+texting = "app"
 
 
 @app.route('/')
@@ -20,21 +23,32 @@ def patientdashboard():
 def patientdashboard1():
     if request.method == 'POST':
         Micro = request.form['Micro']
+        global texting
         texting = sr.voiceinput()
 
     return render_template('Patientdashboard1.html', texting=texting)
 
+
 @app.route('/patientdashboard2', methods=['GET', 'POST'])
 def patientdashboard2():
-    if request.method=='POST':
+    if request.method == 'POST':
         age = request.form['Age']
         bmi = request.form['BMI']
-        op = ir.regrate(age,bmi)
-    return render_template('Patientdashboard2.html',op = op)
+        op = ir.regrate(age, bmi)
+    return render_template('Patientdashboard2.html', op=op)
+
 
 @app.route('/doctordashboard', methods=['GET', 'POST'])
 def doctordashboard():
+    print(texting)
     return render_template('DoctorDashboard.html')
+
+
+@app.route('/doctordashboard2', methods=['GET', 'POST'])
+def doctordashboard2():
+    if request.method=='POST':
+        showQuery = request.form['showQuery']
+    return render_template('DoctorDashboard2.html',texting=texting)
 
 
 if __name__ == "__main__":
